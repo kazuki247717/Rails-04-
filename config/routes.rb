30f11bd01c8e80d487b550db095e82b1690e68ab
kazuki <kazuki247717@icloud.com>
2021-01-09 06:52:32 +0000

    課題提出

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   get 'users/new'
   get 'pages/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
@@ -6,15 +7,22 @@ Rails.application.routes.draw do
 end
 
 Rails.application.routes.draw do
+  get 'sessions/new'
   get 'users/new'
   root 'pages#index'
   get 'pages/help'
 end
 
 Rails.application.routes.draw do
+  get 'sessions/new'
+  
   root 'pages#index'
   get 'pages/help'
   
   
   resources :users
+  
+  get    '/login',   to:  'sessions#new'
+  post   '/login',   to:  'sessions#create'
+  delete '/logout',  to:  'sessions#destroy'
 end
