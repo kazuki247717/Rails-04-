70dc2ba6c8d5e82e9e334280d1e280113a3d64de
kazuki <kazuki247717@icloud.com>
2021-01-20 11:45:56 +0000

    再デプロイ

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'sessions/new'
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'topics#index'
+  root 'pages#index'
   get 'pages/help'
   
   get     '/login',   to: 'sessions#new'
